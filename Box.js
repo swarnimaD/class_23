d3e4a162d7625b062048660c07348832d1a91aa2
swarnimaD <75395710+swarnimaD@users.noreply.github.com>
2020-12-30 16:54:35 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box {
+    constructor(x,y,width,height) {
+            var option = {
+                isStatic : false
+            }
+
+        this.body = Bodies.rectangle(x,y,width,height,option);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
